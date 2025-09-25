4f82d2af6deabb5b49357022a916bde306a0b17b
Isak Arvidsson <iamunds1@volvocars.com>
2025-09-25 08:35:17 +0200

    New build

---
+++ _next/static/chunks/02f822ec1c15735f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/461ad8ea839b6892.js",
+  "static/chunks/turbopack-8208678a1652b94e.js"
+])
---
+++ _next/static/chunks/9dc36da9bc946913.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/461ad8ea839b6892.js",
+  "static/chunks/turbopack-0be1ee0ba8565d47.js"
+])
